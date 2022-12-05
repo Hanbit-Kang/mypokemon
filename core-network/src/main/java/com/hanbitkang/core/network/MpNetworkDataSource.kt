36f4b500e98a238653ca0a2b7790daa27e5bf655
Hanbit-Kang <fchopinof99@gmail.com>
2022-12-06 00:03:04 +0900

    Refactor MpNetworkDataSource to apply DIP, Implement NetworkModule

--- core-network/src/main/java/com/hanbitkang/core/network/MpNetworkDataSource.kt
+++ core-network/src/main/java/com/hanbitkang/core/network/MpNetworkDataSource.kt
@@ -1,14 +1,10 @@
 package com.hanbitkang.core.network
 
+import com.hanbitkang.core.network.model.NetworkPokemon
 import com.hanbitkang.core.network.model.PokemonResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MpNetworkDataSource {
-
-    @GET("pokemon")
-    suspend fun getPokemonList(
-        @Query("limit") limit: Int = 20,
-        @Query("offset") offset: Int = 0
-    ): PokemonResponse
+    suspend fun getPokemonList(): List<NetworkPokemon>
 }
