a3d396ad4505ad82b1fde647ab66a323dd76b518
Hanbit-Kang <fchopinof99@gmail.com>
2022-12-13 22:19:38 +0900

    Implement getPokemonsStream

--- core-data/src/main/java/com/hanbitkang/core/data/repository/OfflineFirstPokemonRepository.kt
+++ core-data/src/main/java/com/hanbitkang/core/data/repository/OfflineFirstPokemonRepository.kt
@@ -1,10 +1,17 @@
 package com.hanbitkang.core.data.repository
 
 import com.hanbitkang.core.data.model.Pokemon
+import com.hanbitkang.core.network.MpNetworkDataSource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
-class OfflineFirstPokemonRepository: PokemonRepository {
+class OfflineFirstPokemonRepository @Inject constructor(
+    private val network: MpNetworkDataSource
+): PokemonRepository {
     override fun getPokemonsStream(): Flow<List<Pokemon>> {
-        TODO("Not yet implemented")
+        return flow {
+            network.getPokemonList()
+        }
     }
 }
