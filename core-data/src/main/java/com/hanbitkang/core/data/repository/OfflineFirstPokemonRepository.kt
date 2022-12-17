2481fff742eb5d431b2b52211c2ea5ba2344b917
Hanbit-Kang <fchopinof99@gmail.com>
2022-12-17 22:00:16 +0900

    Modify PokemonRepository to get data from database

--- core-data/src/main/java/com/hanbitkang/core/data/repository/OfflineFirstPokemonRepository.kt
+++ core-data/src/main/java/com/hanbitkang/core/data/repository/OfflineFirstPokemonRepository.kt
@@ -2,18 +2,21 @@ package com.hanbitkang.core.data.repository
 
 import com.hanbitkang.core.data.model.Pokemon
 import com.hanbitkang.core.data.model.toPokemon
+import com.hanbitkang.core.database.dao.PokemonDao
 import com.hanbitkang.core.network.MpNetworkDataSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class OfflineFirstPokemonRepository @Inject constructor(
-    private val network: MpNetworkDataSource
+    private val pokemonDao: PokemonDao
 ): PokemonRepository {
-    // TODO: Change to flow from DAO
-    override suspend fun getPokemonsStream(): List<Pokemon> {
-        return network.getPokemonList().map {
-            it.toPokemon()
+    override fun getPokemonsStream(): Flow<List<Pokemon>> {
+        return pokemonDao.getPokemonEntityStream().map { pokemonEntities ->
+            pokemonEntities.map {
+                it.toPokemon()
+            }
         }
     }
 }
