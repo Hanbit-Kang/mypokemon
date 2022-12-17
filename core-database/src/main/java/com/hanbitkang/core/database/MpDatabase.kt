2a57fd4268fcbb12c0485983112b9d16fad2ab55
Hanbit-Kang <fchopinof99@gmail.com>
2022-12-17 21:46:39 +0900

    Add MpDatabase

---
+++ core-database/src/main/java/com/hanbitkang/core/database/MpDatabase.kt
@@ -0,0 +1,17 @@
+package com.hanbitkang.core.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.hanbitkang.core.database.dao.PokemonDao
+import com.hanbitkang.core.database.model.PokemonEntity
+
+@Database(
+    entities = [
+        PokemonEntity::class
+    ],
+    version = 1,
+    exportSchema = true,
+)
+abstract class MpDatabase: RoomDatabase() {
+    abstract fun pokemonDao(): PokemonDao
+}
