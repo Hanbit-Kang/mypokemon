04edd6d68ece6efaeee9a889ba000ee4c95c6577
Hanbit-Kang <fchopinof99@gmail.com>
2022-12-28 15:56:21 +0900

    Modify status color to transparent

--- core-designsystem/src/main/java/com/hanbitkang/core/designsystem/theme/Theme.kt
+++ core-designsystem/src/main/java/com/hanbitkang/core/designsystem/theme/Theme.kt
@@ -1,6 +1,7 @@
 package com.hanbitkang.core.designsystem.theme
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -55,8 +56,8 @@ fun MyPokemonTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            (view.context as Activity).window.statusBarColor = Color.TRANSPARENT
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = true
         }
     }
 
