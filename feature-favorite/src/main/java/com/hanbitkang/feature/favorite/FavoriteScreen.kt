fa9284b50eef7d0f11805f99512b45b962a1efa7
Hanbit-Kang <fchopinof99@gmail.com>
2022-12-29 17:28:33 +0900

    Add empty favorite pokemon ui

--- feature-favorite/src/main/java/com/hanbitkang/feature/favorite/FavoriteScreen.kt
+++ feature-favorite/src/main/java/com/hanbitkang/feature/favorite/FavoriteScreen.kt
@@ -1,11 +1,20 @@
 package com.hanbitkang.feature.favorite
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.hanbitkang.core.data.model.Pokemon
 import com.hanbitkang.core.designsystem.component.MpToolbar
@@ -45,7 +54,21 @@ internal fun FavoriteScreen(
                 )
             }
             is FavoriteScreenUiState.Empty -> {
-                // TODO
+                Spacer(modifier = Modifier.height(150.dp))
+                Image(
+                    painter = painterResource(id = com.hanbitkang.core.designsystem.R.drawable.ic_round_hide_source_24),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
+                Spacer(modifier = Modifier.height(15.dp))
+                Text(
+                    text = stringResource(id = R.string.empty_favorite_pokemon),
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally),
+                    fontSize = 18.sp
+                )
             }
             is FavoriteScreenUiState.Loading -> {
                 // TODO
